9ab41c9fd33e596808e8d41a7cc9c487e448bc5c
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-13 16:32:43 +0700

    Make it more like hacker style

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,67 +1,101 @@
-// DATA
+// ===========================================================================
+// DATA STORAGE
 
-const todos = [{
-        text: `Wake up in the morning`
-    },
-    {
-        text: `Take a warm shower`
-    },
-    {
-        text: `Get ready with casual clothes`
-    },
-    {
-        text: `Do some work with the laptop`
-    },
-    {
-        text: `Hack some servers`
-    },
-    {
-        text: `Rule the world`
-    }
-]
+const todos = []
 
+// ===========================================================================
 // DOM ELEMENTS
 
-const todoForm = document.getElementById("todo-form")
+const todoInputForm = document.getElementById("todo-input-form")
+const todoSearchForm = document.getElementById("todo-search-form")
 const todoOutput = document.getElementById("todo-output")
 
-// FUNCTIONS
+const seed = (todos) => {
+    todos.push({
+        text: `Wake up in the morning`,
+        date: new Date()
+    }, {
+        text: `Take a warm shower`,
+        date: new Date()
+    }, {
+        text: `Get ready with casual clothes`,
+        date: new Date()
+    }, {
+        text: `Do some work with the laptop`,
+        date: new Date()
+    }, {
+        text: `Hack some servers`,
+        date: new Date()
+    }, {
+        text: `Rule the world`,
+        date: new Date()
+    })
+}
+
+// ===========================================================================
+// READ / DISPLAY
 
 const createElementFromHTML = (htmlString) => {
     var div = document.createElement("div");
     div.innerHTML = htmlString.trim();
-
     return div.firstChild;
 }
 
-const reloadTodos = () => {
+const createTodoElement = (todo, index) => {
+    return createElementFromHTML(`
+        <div data-id="todo-${index}" class="todo-item">
+            ${todo.text}
+            <span class="date badge badge-pill badge-light">
+                ${moment(todo.date).calendar()}
+            </span>
+            <div class="btn-group">
+                <button class="destroy btn btn-outline-light btn-sm" data-toggle="tooltip" data-placement="top"
+                title="Remove" data-original-title="Remove">
+                    rm
+                </button>
+                <button class="edit btn btn-outline-light btn-sm" data-toggle="tooltip" data-placement="top"
+                title="Edit" data-original-title="Edit">
+                    mv
+                </button>
+            </div>
+        </div>    
+    `)
+}
+
+const display = () => {
     todoOutput.innerHTML = ""
 
     todos.forEach((todo, index) => {
-        const todoElement = createElementFromHTML(`
-            <div class="todo-item" data-id="todo-${index}">
-                ${todo.text}
-                <button class="delete btn btn-outline-light btn-sm">❌</button>
-                <button class="edit btn btn-outline-light btn-sm">✍</button>
-            </div>    
-        `)
-
+        const todoElement = createTodoElement(todo, index)
         todoOutput.append(todoElement)
     })
+
+    $('[data-toggle="tooltip"]').tooltip()
 }
 
+// ===========================================================================
 // LISTENERS
 
-todoForm.addEventListener("submit", (event) => {
+todoInputForm.addEventListener("submit", (event) => {
     event.preventDefault()
-    const todoText = document.getElementById("todo-text").value
+    const todoInputText = document.getElementById("todo-input-text").value
     todos.push({
-        text: todoText
+        text: todoInputText,
+        date: new Date()
     })
 
-    if (todoText !== "") reloadTodos()
+    if (todoInputText !== "") display()
 })
 
+todoSearchForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
+// ===========================================================================
 // INITIALIZER
 
-reloadTodos()
+// SEEDER
+seed(todos)
+
+// INITIAL DISPLAY
+display()
