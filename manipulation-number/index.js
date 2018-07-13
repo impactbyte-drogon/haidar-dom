db982728b9936f5d694f8677067f1926eeaac67e
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-13 09:48:40 +0700

    Implement manipulation number project

---
+++ manipulation-number/index.js
@@ -0,0 +1,13 @@
+console.log("=== Manipulation Number ===");
+
+const titleElement = document.getElementById("title")
+
+console.log("title", titleElement);
+
+const getNumbers = () => {
+    const numberFirst = document.getElementById("number-first").value
+    const numberSecond = document.getElementById("number-second").value
+
+    console.log("numberFirst:", numberFirst);
+    console.log("numberSecond:", numberSecond);
+}
