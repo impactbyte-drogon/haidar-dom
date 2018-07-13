87738abd34b42ee152d42e8318fc4fcea7444ede
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-13 11:00:17 +0700

    Initialize comparison number

---
+++ comparison-number/index.js
@@ -0,0 +1,15 @@
+console.log("=== Compare Number ===");
+
+const title = document.getElementById("title")
+// console.log("title", titleElement);
+
+const output = document.getElementById("output-paragraph")
+
+const compareNumbers = () => {
+    const numberFirst = document.getElementById("number-first").value
+    const numberSecond = document.getElementById("number-second").value
+
+    output.innerText = `
+    ...
+    `
+}
